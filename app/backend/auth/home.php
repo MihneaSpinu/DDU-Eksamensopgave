cbe508bd0835dc7e0815cb016780126b43d9509c
Gunnar <gunnarmani04@gmail.com>
2024-03-22 14:58:00 +0100

    Schedule arbejde

--- app/backend/auth/home.php
+++ app/backend/auth/home.php
@@ -2,35 +2,35 @@
 
 $submissions = $db->get('submissions', array('student_ID', '=', $user->data()->uid))->results();
 
-// $schedule = array();
-// foreach ($all_subjects as $subject) {
-//     $schedules = $db->get('schedule', array('subject_class_ID', '=', $subject->subject_class_ID))->results();
-//     foreach ($schedules as $s) {
-//         $s->subject_name = $db->get('subject', array('subject_ID', '=', $subject->subject_ID))->first()->subject_name;
-//         $schedule[] = $s;
-//     }
-// }
+$schedule = array();
+foreach ($all_subjects as $subject) {
+    $schedules = $db->get('schedule', array('subject_class_ID', '=', $subject->subject_class_ID))->results();
+    foreach ($schedules as $s) {
+        $s->subject_name = $db->get('subject', array('subject_ID', '=', $subject->subject_ID))->first()->subject_name;
+        $schedule[] = $s;
+    }
+}
 
-// $today = date('D');
-// $today_schedule = array();
-// foreach ($schedule as $s) {
-//     if (date('D', strtotime($s->date)) == $today) {
-//         //Add $s to $today_schedule
-//         $today_schedule[] = $s;
-//     }
-//     $s->time = date('H:i', strtotime($s->date));
-//     $s->date = date('Y-m-d', strtotime($s->date));
-//     $s->minute = date('H', strtotime($s->time)) * 60 + date('i', strtotime($s->time));
-// }
+$today = date('D');
+$today_schedule = array();
+foreach ($schedule as $s) {
+    if (date('D', strtotime($s->date)) == $today) {
+        //Add $s to $today_schedule
+        $today_schedule[] = $s;
+    }
+    $s->time = date('H:i', strtotime($s->date));
+    $s->date = date('Y-m-d', strtotime($s->date));
+    $s->first_minute = (strtotime($s->time) - strtotime(date('Y-m-d 00:00'))) / 60;
+    $s->last_minute = $s->first_minute + $s->period * 60;
+    //Get teacher from subject class
+    $teacher = $db->get('subject_class', array('subject_class_ID', '=', $s->subject_class_ID))->first()->subject_teacher_ID;
+    $s->teacher = $db->get('users', array('uid', '=', $teacher))->first()->initials;
+}
 
-// //Sort by time
-// usort($today_schedule, function ($a, $b) {
-//     return strtotime($a->time) - strtotime($b->time);
-// });
+//Sort by time
+usort($today_schedule, function ($a, $b) {
+    return strtotime($a->time) - strtotime($b->time);
+});
 
-
-// //First minute from first object from $today_schedule from time
-// $first_minute = date('H', strtotime($today_schedule[0]->time)) * 60 + date('i', strtotime($today_schedule[0]->time));
-// $last_minute = date('H', strtotime($today_schedule[count($today_schedule) - 1]->time)) * 60 + date('i', strtotime($today_schedule[count($today_schedule) - 1]->time));
-// $first_hour = date('H', strtotime($today_schedule[0]->time));
-// $amount_of_minutes = $last_minute - $first_minute;
+$first_hour = floor($today_schedule[0]->first_minute) / 60;
+$last_hour = ceil($today_schedule[count($today_schedule) - 1]->last_minute) / 60;
--- app/frontend/pages/home.php
+++ app/frontend/pages/home.php
@@ -19,7 +19,82 @@
           Nyheder
         </div>
       </div> -->
-    </div>  
+    </div>
   </div>
 
+</div>
+
+<style>
+  .schedule {
+    display: grid;
+    grid-template-columns: 60px 1fr;
+    grid-template-rows: repeat(1440, 1px);
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    position: relative;
+  }
+
+  .timestamps,
+  .events {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-auto-rows: 1px;
+    position: relative;
+  }
+
+  .timestamp {
+    text-align: center;
+  }
+
+  .event {
+    background-color: #e9ecef;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+</style>
+<script>
+  //Make grid-template-rows start from first hour of today_schedule[0] and end at last hour of today_schedule[today_schedule.length - 1]
+  const first_hour = <?php echo $first_hour; ?>;
+  const last_hour = <?php echo $last_hour; ?>;
+  const schedule = document.querySelector('.schedule');
+  schedule.style.gridTemplateRows = `repeat(${last_hour - first_hour}, 1px)`;
+  
+</script>
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <div class="schedule">
+        <!-- Timestamps -->
+        <div class="timestamps">
+          <?php
+          //Create timestamps from first hour to last hour based on today_schedule
+          for ($i = $first_hour; $i < $last_hour; $i++) {
+            echo "<div class='timestamp' style='grid-row: " . $i * 60 . " / " . ($i + 1) * 60 . ";'>" . date('H:i', strtotime('00:00') + $i * 60 * 60) . "</div>";
+          }
+          ?>
+        </div>
+        <div class="events">
+          <?php foreach ($today_schedule as $event) : ?>
+
+            <div class='event ml-2 py-2' style='grid-row: <?php echo $event->first_minute; ?>/<?php echo $event->last_minute; ?>;'>
+              <div class="row px-2">
+                <div class="col-8">
+                  <h6><?php echo $event->subject_name; ?></h6>
+                  <a><?php echo $event->time . " - " . date('H:i', strtotime($event->time) + $event->period * 60 * 60); ?></a>
+                  <a><?php echo $event->teacher; ?></a>
+                </div>
+                <div class="col-4">
+                  <a>Room: <?php echo $event->room; ?></a>
+                </div>
+              </div>
+            </div>
+          <?php endforeach;
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
